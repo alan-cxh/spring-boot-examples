6da2b4e8836b9deabc5b57b21146534ce16135dd
alan_chen <alanan_chen@sjtu.edu.cn>
2018-12-26 19:34:30 +0800

    添加项目文档描述

---
+++ spring-boot-helloworld/src/main/java/com/alan/springboothelloworld/helloworld/entity/User.java
@@ -0,0 +1,32 @@
+package com.alan.springboothelloworld.helloworld.entity;
+
+
+public class User {
+    private String id;
+    private String username;
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
