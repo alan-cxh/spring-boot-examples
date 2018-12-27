349ce220a2a9f1b588d2ef6234c8f0a0698b3129
alan_chen <alanan_chen@sjtu.edu.cn>
2018-12-27 16:39:37 +0800

    配置druid连接池的监控

--- spring-boot-helloworld/src/main/java/com/alan/springboothelloworld/SpringBootHelloworldApplication.java
+++ spring-boot-helloworld/src/main/java/com/alan/springboothelloworld/SpringBootHelloworldApplication.java
@@ -1,19 +1,28 @@
 package com.alan.springboothelloworld;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableScheduling //  定时任务开启
 @SpringBootApplication
 @EnableTransactionManagement // 添加事务注解
-
+@ServletComponentScan
 public class SpringBootHelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHelloworldApplication.class, args);
     }
+
+   /* @Bean("duridDatasource")
+    @ConfigurationProperties(prefix="application-dev.properties")
+    public DruidDataSource druidDataSource() { return new DruidDataSource();};
+*/
 
 }
 
